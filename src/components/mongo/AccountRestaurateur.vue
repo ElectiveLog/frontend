ac840d355210a33f386575964f799198d577e0e4
matheobrise <matheo.brise17@gmail.com>
2022-06-30 16:28:32 +0200

    y

--- src/components/mongo/AccountRestaurateur.vue
+++ src/components/mongo/AccountRestaurateur.vue
@@ -324,55 +324,55 @@ export default {
       fields: [
         {
           key: "Commande",
-          label: "Commande",
+          label: "Commande"
         },
         {
           key: "prix",
-          label: "Prix",
+          label: "Prix"
         },
         { key: "articles", label: "Articles" },
         {
           key: "livreur",
-          label: "Livreur",
+          label: "Livreur"
         },
         {
           key: "client",
-          label: "client",
+          label: "client"
         },
         {
           key: "status",
-          label: "Status",
+          label: "Status"
         },
         {
           key: "actions",
-          label: "Action",
+          label: "Action"
         },
-        { key: "show_details", label: "Details" },
+        { key: "show_details", label: "Details" }
       ],
       fieldsMore: [
         {
           key: "Commande",
-          label: "Commande",
+          label: "Commande"
         },
         {
           key: "prix",
-          label: "Prix",
+          label: "Prix"
         },
         { key: "articles", label: "Articles" },
         { key: "livreur", label: "Livreur" },
         { key: "client", label: "Client" },
         { key: "status", label: "Status" },
-        { key: "show_details", label: "Details" },
+        { key: "show_details", label: "Details" }
       ],
       inProgressCommandes: [],
-      historyCommandes: [],
+      historyCommandes: []
     };
   },
   methods: {
     handleEditStatus(commande) {
       this.$socket.emit("OrderAcceptRestaurant", "1");
       var data = JSON.stringify({
-        state: "preparation",
+        state: "preparation"
       });
 
       var config = {
@@ -383,11 +383,10 @@ export default {
           commande.id,
         headers: {
           "X-Server-Select": "mongo",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpvaG4iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NTU3NTg3MjUsImV4cCI6MTY1NjM2MzUyNX0.vHdiEc98ELrbBDbeZeG-851qS_SLSHJW8HDJX7mPgjs",
-          "Content-Type": "application/json",
+          Authorization: "Bearer" + user.accessToken,
+          "Content-Type": "application/json"
         },
-        data: data,
+        data: data
       };
 
       axios(config)
@@ -397,11 +396,11 @@ export default {
             title: "Commande validée",
             type: "success",
             text: "La commande a été validée avec succès",
-            duration: 8000,
+            duration: 8000
           });
           location.reload();
         })
-        .catch(function (error) {
+        .catch(function(error) {
           console.log(error);
         });
     },
@@ -414,9 +413,9 @@ export default {
           ":8080/users/" +
           payloadUser.userId,
         headers: {
-          Authorization: "Bearer " + user.accessToken,
+          Authorization: "Bearer " + user.accessToken
         },
-        data: this.userData,
+        data: this.userData
       };
 
       axios(config)
@@ -426,10 +425,10 @@ export default {
             title: "Modification réussie",
             type: "success",
             text: "Vos modifications ont été enregistrées",
-            duration: 8000,
+            duration: 8000
           })
         )
-        .catch(function (error) {
+        .catch(function(error) {
           console.log(error);
         });
 
@@ -437,19 +436,19 @@ export default {
         method: "post",
         url: window.location.origin.split(":80")[0] + ":8080/api/logs/create",
         headers: {
-          "X-Server-Select": "mongo",
+          "X-Server-Select": "mongo"
         },
         data: {
           type: "Modification",
           description:
-            payloadUser.email + "(Restaurateur) a modifié son compte.",
-        },
+            payloadUser.email + "(Restaurateur) a modifié son compte."
+        }
       };
       axios(configLog)
-        .then((response) => {
+        .then(response => {
           console.log(JSON.stringify(response.data));
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
     },
@@ -459,19 +458,19 @@ export default {
         method: "post",
         url: window.location.origin.split(":80")[0] + ":8080/api/logs/create",
         headers: {
-          "X-Server-Select": "mongo",
+          "X-Server-Select": "mongo"
         },
         data: {
           type: "Suppression",
           description:
-            payloadUser.email + "(Restaurateur) a supprimé son compte.",
-        },
+            payloadUser.email + "(Restaurateur) a supprimé son compte."
+        }
       };
       axios(configLog)
-        .then((response) => {
+        .then(response => {
           console.log(JSON.stringify(response.data));
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
       var config = {
@@ -481,8 +480,8 @@ export default {
           ":8080/users/" +
           payloadUser.userId,
         headers: {
-          Authorization: "Bearer " + user.accessToken,
-        },
+          Authorization: "Bearer " + user.accessToken
+        }
       };
 
       axios(config)
@@ -492,18 +491,18 @@ export default {
             title: "Suppression réussie",
             type: "success",
             text: "Votre compte a été supprimé",
-            duration: 8000,
+            duration: 8000
           });
           this.$store.dispatch("auth/logout");
           this.$router.push("/login");
         })
-        .catch(function (error) {
+        .catch(function(error) {
           console.log(error);
         });
     },
     decodeToken(token) {
       return jwt_decode(token);
-    },
+    }
   },
   async created() {
     const payloadUser = this.decodeToken(user.accessToken);
@@ -514,15 +513,15 @@ export default {
         ":8080/users/" +
         payloadUser.userId,
       headers: {
-        Authorization: "Bearer " + user.accessToken,
-      },
+        Authorization: "Bearer " + user.accessToken
+      }
     };
 
     await axios(config)
-      .then((response) => {
+      .then(response => {
         this.userData = response.data;
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
       });
 
@@ -533,16 +532,16 @@ export default {
         ":8080/api/restaurants/restaurateur/" +
         payloadUser.userId,
       headers: {
-        "X-Server-Select": "mongo",
-      },
+        "X-Server-Select": "mongo"
+      }
     };
 
-    await axios(configRestaurant).then(async (response) => {
+    await axios(configRestaurant).then(async response => {
       var i = 1;
       var y = 1;
       var z = 1;
       response.data.restaurants
-        .forEach(async (restaurant) => {
+        .forEach(async restaurant => {
           var config = {
             method: "get",
             url:
@@ -550,13 +549,13 @@ export default {
               ":8080/api/orders/restaurant/" +
               restaurant._id,
             headers: {
-              "X-Server-Select": "mongo",
-            },
+              "X-Server-Select": "mongo"
+            }
           };
 
           await axios(config)
-            .then((response) => {
-              response.data.order.forEach(async (order) => {
+            .then(response => {
+              response.data.order.forEach(async order => {
                 var priceCommande = 0;
                 var configLivreur = {
                   method: "get",
@@ -565,15 +564,15 @@ export default {
                     ":8080/users/" +
                     order.idLivreur,
                   headers: {
-                    Authorization: "Bearer " + user.accessToken,
-                  },
+                    Authorization: "Bearer " + user.accessToken
+                  }
                 };
 
                 await axios(configLivreur)
-                  .then((response) => {
+                  .then(response => {
                     order.idLivreur = response.data.name;
                   })
-                  .catch(function (error) {
+                  .catch(function(error) {
                     console.log(error);
                   });
 
@@ -584,12 +583,12 @@ export default {
                     ":8080/users/" +
                     order.idClient,
                   headers: {
-                    Authorization: "Bearer " + user.accessToken,
-                  },
+                    Authorization: "Bearer " + user.accessToken
+                  }
                 };
 
                 await axios(configClient)
-                  .then((response) => {
+                  .then(response => {
                     order.idClient = response.data.name;
                     order.streetNumber = response.data.streetNumber;
                     order.address = response.data.address;
@@ -598,11 +597,11 @@ export default {
                     order.phoneNumber = response.data.phoneNumber;
                     order.sponsorshipCode = response.data.sponsorshipCode;
                   })
-                  .catch(function (error) {
+                  .catch(function(error) {
                     console.log(error);
                   });
                 var articlesNames = "";
-                order.articles.forEach((article) => {
+                order.articles.forEach(article => {
                   priceCommande += article.price;
                   articlesNames += article.name + ", ";
                 });
@@ -631,7 +630,10 @@ export default {
                     status: order.state,
                     parnainage: order.sponsorshipCode,
                     date: order.createdAt.split("T")[0],
-                    heure: order.createdAt.split("T").pop().split(".")[0],
+                    heure: order.createdAt
+                      .split("T")
+                      .pop()
+                      .split(".")[0]
                   });
                   i++;
                 } else if (
@@ -652,7 +654,10 @@ export default {
                     status: order.state,
                     parnainage: order.sponsorshipCode,
                     date: order.createdAt.split("T")[0],
-                    heure: order.createdAt.split("T").pop().split(".")[0],
+                    heure: order.createdAt
+                      .split("T")
+                      .pop()
+                      .split(".")[0]
                   });
 
                   y++;
@@ -671,21 +676,24 @@ export default {
                     status: "Livrée",
                     parnainage: order.sponsorshipCode,
                     date: order.createdAt.split("T")[0],
-                    heure: order.createdAt.split("T").pop().split(".")[0],
+                    heure: order.createdAt
+                      .split("T")
+                      .pop()
+                      .split(".")[0]
                   });
 
                   z++;
                 }
               });
             })
-            .catch(function (error) {
+            .catch(function(error) {
               console.log(error);
             });
         })
-        .catch(function (error) {
+        .catch(function(error) {
           console.log(error);
         });
     });
-  },
+  }
 };
 </script>
